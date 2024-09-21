3634d03c08e79ee34a0809fd36184172fa623fff
Daniel Sanchez Gonzalez <sanchezgonzalezdaniel3@gmail.com>
2024-09-21 17:46:15 -0600

    Uso de la libreria Junit

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -1,16 +1,14 @@
 package com.platzi.javatests.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
-public static void main(String[] args) {
 
-    assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
-    assertEquals(StringUtil.repeat("hola", 1), "hola");
+    @Test
+    public void testRepeat() {
+    Assert.assertEquals("holaholahola",StringUtil.repeat("hola", 3));
+    Assert.assertEquals("hola",StringUtil.repeat("hola", 1));
 
-}
-
-    private static void assertEquals(String actualValue, String expectedValue) {
-        if (!actualValue.equals(expectedValue)) {
-            throw new RuntimeException(actualValue + " is not equal to expected" + expectedValue);
-        }
     }
 }
