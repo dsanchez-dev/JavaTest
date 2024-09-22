bf8b653258e00d4246b58676a02378b199f647f6
Daniel Sanchez Gonzalez <sanchezgonzalezdaniel3@gmail.com>
2024-09-22 17:58:17 -0600

    Análisis de los test y mejoras en la estructura de los test

--- src/test/java/com/platzi/javatests/payments/PaymentProcessorTest.java
+++ src/test/java/com/platzi/javatests/payments/PaymentProcessorTest.java
@@ -1,19 +1,27 @@
 package com.platzi.javatests.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setup() {
+        //La finalidad de este método es inicializar los objetos que se van a utilizar en los tests
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
 
     @Test
     public void payment_is_correct() {
-
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
-        Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+        //partes de un test:  Preparación de los objetos, ejecución del método, verificación del resultado
+        Mockito.when(paymentGateway.requestPayment(Mockito.any())).
+                thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
 
         assertTrue( paymentProcessor.makePaymente(1000));
     }
@@ -21,11 +29,8 @@ public class PaymentProcessorTest {
     @Test
     public void payment_is_wrong() {
 
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).
                 thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertFalse( paymentProcessor.makePaymente(1000));
     }
