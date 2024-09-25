7d8e5813bf0819a80d48bec88628a09ca805b219
Daniel Sanchez Gonzalez <sanchezgonzalezdaniel3@gmail.com>
2024-09-24 21:58:35 -0600

    Test de integración con base de datos: para guardar películas y búsqueda de películas individuales

--- src/main/java/com/platzi/javatests/movies/data/MovieRepositoryJdbc.java
+++ src/main/java/com/platzi/javatests/movies/data/MovieRepositoryJdbc.java
@@ -17,7 +17,10 @@ public class MovieRepositoryJdbc implements MovieRepository {
 
     @Override
     public Movie findById(long id) {
-        return null;
+
+        Object[] args = {id};
+
+        return jdbcTemplate.queryForObject("select * from movies where id = ?", args,movieMapper);
     }
 
     @Override
@@ -27,6 +30,8 @@ public class MovieRepositoryJdbc implements MovieRepository {
 
     @Override
     public Void saveOrUpdate(Movie movie) {
+        jdbcTemplate.update("insert into movies (name, minutes, genre) values (?, ?, ?)",
+                movie.getName(), movie.getMinutes(), movie.getGenre().toString());
         return null;
     }
 
